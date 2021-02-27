89534e39353f6e716b85e6667dcca6d72b12c327
maxgallup <m.gallup@student.vu.nl>
2021-02-27 13:46:52 +0100

    routes working correctly

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const app = require('express')()
+var app = require('express')();
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.render('index', {view:'home'});
 });
 
-app.get('/profile/:name', (req, res) => {
-    res.send("you are viewing: " + req.params.name);
+app.get('/posts', (req, res) => {
+    res.render('index', {view:'posts'});
+});
+
+app.get('/posts/:name', (req, res) => {
+    res.render('index', {view: 'posts/' + req.params.name});
+});
+
+app.get('/about', (req, res) => {
+    res.render('index', {view:'about'});
+});
+
+app.get('/*', (req, res) => {
+    res.render('index', {view:'404'});
 });
 
 
-app.listen(8080, () => console.log("webserver on 8080"));
 
+app.listen(8080, console.log("webserver on 8080"));
